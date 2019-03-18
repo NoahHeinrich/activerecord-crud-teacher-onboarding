4dace999f9b28d50f84f6d971f83656075f69ec6
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-18 20:42:10 +0000

    Automatically backed up by Learn

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,20 @@
 class Movie < ActiveRecord::Base
+  attr_accessor :title, :release_date, :director, :lead, :in_theaters
+  def initialize(title, release_date = nil, director = nil, lead = nil, in_theaters = false)
+    @release_date = release_date
+    @title = title
+    @director = director
+    @lead = lead
+    @in_theaters = in_theaters
+  end
+  
+  def in_theaters?
+    self.in_theaters
+  end
+  
+  def save
+  end
+  
+  def find_by(attributes)
+  end
 end
